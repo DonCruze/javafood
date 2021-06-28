67a8d19c03ef5ce03f38b304f523b0b1bd8132b7
Ravshanjon Ismoilov <ismoilov.ravshanjon@gmail.com>
2021-06-28 21:05:23 +0500

    Add: Menu & Menu Item

--- src/main/java/uz/chayxana/javafood/menu/MenuRepo.java
+++ src/main/java/uz/chayxana/javafood/menu/MenuRepo.java
@@ -2,7 +2,12 @@ package uz.chayxana.javafood.menu;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import uz.chayxana.javafood.organization.Organization;
+
+import java.util.List;
 
 @Repository
 public interface MenuRepo extends JpaRepository<Menu, Long> {
+
+    List<Menu> findAllByOrganization(Organization organization);
 }
